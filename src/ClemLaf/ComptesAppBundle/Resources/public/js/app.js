f97ec0b65f9d375108b447a39c27434ebf73e99a
Clément Lafaurie <grainderiz@melix.net>
2016-03-12 15:46:18 +0100

    petites optimisation d'affichage de la page web
    > css pour obtenir des listes qui s'aggrandissent quand on pointe dessus

--- src/ClemLaf/ComptesAppBundle/Resources/public/js/app.js
+++ src/ClemLaf/ComptesAppBundle/Resources/public/js/app.js
@@ -15,3 +15,35 @@ for(var i=0; i<ff.elements.length;i++){
 }
 $("#entree_date1").datepicker({dateFormat:"yy-mm-dd"});
 $("#entree_date2").datepicker({dateFormat:"yy-mm-dd"});
+$("#entree_cpS").on("mouseleave",function(){
+	setTimeout(function(){
+		$s=$('#entree_cpS');
+		if($s.find(':selected').length>0){
+			opttop=$s.find(':selected').offset().top;
+			stop=$s.offset().top;
+			$s.scrollTop($s.scrollTop()+(opttop-stop));
+		}
+	},100);
+});
+$("#entree_cpD").on("mouseleave",function(){
+	setTimeout(function(){
+		$s=$('#entree_cpD');
+		if($s.find(':selected').length>0){
+			opttop=$s.find(':selected').offset().top;
+			stop=$s.offset().top;
+			$s.scrollTop($s.scrollTop()+(opttop-stop));
+		}
+	},100);
+});
+$("#entree_point").on('click',function(){
+	if(this.value=='_')
+		this.value='x';
+	else if(this.value=='x')
+		this.value=' ';
+	else
+		this.value='_';
+	this.onchange();
+})
+$("#entree_point").attr('autocomplete','off');
+$("#entree_point").attr('readonly','');
+$("#entree_type").parent().addClass('type_row');
